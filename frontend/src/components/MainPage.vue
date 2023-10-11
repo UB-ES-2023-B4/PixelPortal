bdadf88e78b09127003d40a70fd46693e043957c
martialtimira <zignar1999@gmail.com>
2023-10-11 12:17:45 +0200

    Created Component and variables

--- frontend/src/components/MainPage.vue
+++ frontend/src/components/MainPage.vue
@@ -83,6 +83,8 @@ export default {
         },
       ],
       showDropdown: false,
+      username: "notLoggedIn",
+      token: null,
     };
   },
   computed: {
---
+++ frontend/src/components/UploadImagePopup.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "UploadImagePopup",
+  data() {
+    return {
+      username: "notLoggedIn",
+      token: null,
+    };
+  },
+};
+</script>
+
+<style></style>
