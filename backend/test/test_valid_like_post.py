584bd43726ac14f5168d15bd10740dc2fb862e67
CarlosGD <cgomezdo22@alumnes.ub.edu>
2023-11-17 18:07:32 +0100

    QA test

--- backend/test/test_valid_like_post.py
+++ backend/test/test_valid_like_post.py
@@ -1,20 +1,32 @@
 import pytest
 
-@pytest.mark.parametrize("like_data", [
+user_data_list = [
+    {"nombre": "testuser",      "email": "test@example.com",        "contrasena": "testpassword"},
+    {"nombre": "testuser2",     "email": "test@gmail.com",          "contrasena": "testpassword"},
+    {"nombre": "testuser3",     "email": "test.ub@alumnes.ub.edu",  "contrasena": "testpassword"},
+    {"nombre": "Pepe",          "email": "pepe@gmail.com",          "contrasena": "Qwer!234"}
+]
+
+def register(test_client, user_data):
+    response = test_client.post("/usuario/", json=user_data)
+    assert response.status_code == 200, response.json()
+    return response.json()['user']['id']
+
+likes_data_list = [
     {"usuario_id": "1", "publicacion_id" : "1"},
 	{"usuario_id": "1", "publicacion_id": "2"},
 	{"usuario_id": "1", "publicacion_id": "3"},
 	{"usuario_id": "1", "publicacion_id": "4"}
-])
+]
+@pytest.mark.parametrize("count", [0, 1, 2, 3])
+def test_post_like(test_client, count):
+    user_id_list = []
+    response = test_client.get(f"/usuario/{likes_data_list[count]}/")
+    response_data = response.json()
+    assert response.status_code == 200, response_data
 
-def test_post_like(test_client, like_data):
-    user_data = {
-        "nombre": "Jonadan",
-        "email": "jona@hotmail.com",
-        "contrasena": "Awsome123$"
-        }
 
-    login_data = {"email": "jona@hotmail.com", "contrasena": "Awsome123$"}
+    login_data = {"email": response.json()["email"], "contrasena": response.json()["contrasena"]}
     response = test_client.post("/login", json=login_data)
     assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}. Response: {response.json()}"
     access_token = response.json()["access_token"]
