3dc2ce8c7d5f771b89d8b49e07f61e7f519ce796
jhwan818 <devjhwan818@gmail.com>
2023-12-12 22:31:57 +0100

    Finish show following posts test

---
+++ frontend/cypress/e2e/20_show_following_posts.spec.cy.js
@@ -0,0 +1,25 @@
+describe('Test show following posts', () => {
+	beforeEach(() => {
+		cy.visit('http://localhost:8080');
+		cy.get('.login-show input[placeholder="Username"]').type('test@gmail.com');
+		cy.get('.login-show input[placeholder="Password"]').type('test');
+		cy.get('.login-show input[type="button"][value="Login"]').click();
+		cy.url().should('include', '/home');
+		cy.wait(1500);
+	});
+
+	it('test show following post', () => {
+		cy.contains('button.post-button', 'Search User').click();
+		cy.wait(1500);
+		cy.get('.user-container .user').first().click();
+		cy.url().should('include', '/user');
+		cy.wait(500);
+		cy.get('.page-button').contains('Follow').click();
+		cy.get('[data-cy=home-button]').click();
+		cy.wait(1500);
+		cy.get('button.post-button').contains('Following Posts').click();
+		cy.wait(500);
+		cy.get('.image-container .images .image-card').should('have.length.gte', 2);
+		cy.get('button.post-button').contains('Following Posts').click();
+	})
+});
--- frontend/run_cypress_test.sh
+++
@@ -1,4 +0,0 @@
-# !/bin/bash
-npx cypress run --spec cypress/e2e/register.spec.cy.js
-npx cypress run --spec cypress/e2e/login.spec.cy.js
-npx cypress run --spec cypress/e2e/upload_image.spec.cy.js
